6a4df8d54c6341d9fa136c9007160daa908b0bc8
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-11 15:38:23 -0600

    fixed app wrapping in index.js, removed unneeded functions from app.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {Reducer} from './reducers/Reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+const store = createStore(Reducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
