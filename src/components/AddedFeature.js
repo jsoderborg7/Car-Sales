ee0ba0242896074554dad92186e07edd765b6925
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-11 16:32:01 -0600

    addtional features not showing up, I think I need to add more reducers

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {removeItem} from '../actions/Actions';
 
 const AddedFeature = props => {
+
+  let removeFeature = e =>{
+    e.preventDefault();
+    props.removeItem(props.feature.price);
+  }
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={removeFeature}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state =>{
+  return{
+    data: state.store.feature
+  };
+};
+export default connect(mapStateToProps, {removeItem})(AddedFeature);
