a9a246551caf3816bba98f070bae78011caef0d6
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-11 21:10:15 -0600

    changed the removeFeatures to removeItem in addedFeature.js and addedFeatures.js

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const AddedFeature = props => {
-
+  console.log(props);
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={() => props.removeFeature(props.feature)}>X</button>
+      <button className="button" onClick={() => props.removeItem(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
